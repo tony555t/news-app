1f2f4c5d52e568ecd0eb3bcb79c700b4404e4c48
tony555t <your-email@example.com>
2025-09-04 16:55:20 +0300

    styling  the modal

--- src/components/NewsModal.jsx
+++ src/components/NewsModal.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import demoImg from '../assets/images/demo.jpg';
 
 const NewsModal = () => {
   return <div className="modal-overlay">
     <div className="modal-content">
     <span className="close-button">
-      i.fa=<solid className="fa-xmark"></solid>
+      <i className="fa-solid fa-xmark"></i>
     </span>
+    <img src={demoImg}alt="demo" className="modal-image "/>
+    <h2 className="modal-title">lorem ipsum dolor</h2>
+    <p className="modal-source"> source:the guardian</p>
+    <p className="modal-date">jun 24,2024, 04:15</p>
+    <p className="modal-content-text"></p>
+    <a href="" className="read-more-link">read more</a>
   </div>
   </div>
 }
