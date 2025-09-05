d7770e001b2488bc448b0bee64f741cd64ee3476
tony555t <your-email@example.com>
2025-09-05 12:35:33 +0300

    modal bookmark

--- src/components/Bookmarks.jsx
+++ src/components/Bookmarks.jsx
@@ -3,25 +3,34 @@ import './Modal.css'
 import demoImg from '../assets/images/demo.jpg'
 import './Bookmarks.css'
  
-const Bookmark = ({ show, Bookmark, onClose, onselectArticle,onDeleteBookmark})=>{
+const Bookmark = ({ show, Bookmark, onClose, onSelectArticle, onDeleteBookmark}) => {
     if(!show){
         return null
     }
     return(
         <div className="modal-overlay">
             <div className="modal-content">
-                <span className="close-button">
+                <span className="close-button" onClick={onClose}>
                     <i className='fa-solid fa-xmark'></i>
                 </span>
                 <h2 className='bookmark-heading'>bookmark news</h2>
                 <div className="bookmark-list">
-                    
+                    {Bookmark.map((article,index)=>(
+                        <div className="bookmark-item" key={index} onClick={() => onSelectArticle(article)}>
+                            <img src={article.image || demoImg} alt={article.title} />
+                            <h3>{article.title}</h3>
+                            <span className='delete-button' onClick={(e)=>{
+                                e.stopPropagation()
+                                onDeleteBookmark(article)
+                            }}>
+                                <i className="fa-regular fa-circle-xmark"></i>
+                            </span>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
     )
 }
 
-
-
-export default Bookmarks
+export default Bookmark
