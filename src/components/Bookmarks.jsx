ad0e9940facc0c77366c95bd90f4d99135c51481
tony555t <your-email@example.com>
2025-09-05 12:24:50 +0300

    bookmark modal

--- src/components/Bookmarks.jsx
+++ src/components/Bookmarks.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-
-function Bookmarks() {
-  return (
-    <div>Bookmarks</div>
-  )
+import './Modal.css'
+import demoImg from '../assets/images/demo.jpg'
+import './Bookmarks.css'
+ 
+const Bookmark = ({ show, Bookmark, onClose, onselectArticle,onDeleteBookmark})=>{
+    if(!show){
+        return null
+    }
+    return(
+        <div className="modal-overlay">
+            <div className="modal-content">
+                <span className="close-button">
+                    <i className='fa-solid fa-xmark'></i>
+                </span>
+                <h2 className='bookmark-heading'>bookmark news</h2>
+                <div className="bookmark-list">
+                    
+                </div>
+            </div>
+        </div>
+    )
 }
+
+
 
 export default Bookmarks
